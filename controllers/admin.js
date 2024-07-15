c76cd7c0c7cf6db612d66666c1243e88270833ef
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-07-15 19:36:14 +0530

    product model completely updated to nosql

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const { ObjectId, mongoDb } = require('mongodb');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -13,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product  = new Product(title,price,description,imageUrl);
+  const product = new Product(title, price, description, imageUrl);
 
   product.
     save()
@@ -33,11 +34,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
+  Product.findById(prodId)
+
     // Product.findById(prodId)
-    .then(products => {
-      const product = products[0];
+    .then(product => {
+
       if (!product) {
         return res.redirect('/');
       }
@@ -57,14 +58,10 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+  const product = new Product(updatedTitle, updatedPrice,  updatedDesc,updatedImageUrl, prodId);
+  product
+    .save()
+   
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -73,8 +70,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
+
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -87,11 +84,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
-    .then(product => {
-      return product.destroy();
-    })
-    .then(result => {
+  Product.deleteById(prodId)
+    
+    .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
--- models/product.js
+++ models/product.js
@@ -4,16 +4,23 @@ const mongoDb = require('mongodb');
 
 
 class Product {
-    constructor(title,price,description,imageUrl){
+    constructor(title,price,description,imageUrl,id){
       this.title = title;
       this.price = price;
       this.description = description;
       this.imageUrl = imageUrl;
+      this._id = id ? new mongoDb.ObjectId( id) : null;
     }
 
     save(){
       const db = getDb();
-      return db.collection('products').insertOne(this)
+      let dbOp;
+      if(this._id){
+        dbOp =  db.collection('products').updateOne({_id: this._id}, {$set : this}) ;
+      }else{
+        dbOp = db.collection('products').insertOne(this)
+      }
+      return dbOp
       .then(result => {
         console.log(result);
       })
@@ -44,6 +51,20 @@ class Product {
       .then(product => {
         console.log(product)
         return product;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+    }
+
+    static deleteById(prodId){
+      const db = getDb();
+      return db.collection('products')
+      .deleteOne({_id : new mongoDb.ObjectId(prodId)})
+      .then(result => {
+        console.log(result)
+        
       })
       .catch(err => {
         console.log(err);
