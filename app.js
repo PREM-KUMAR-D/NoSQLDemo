8eba702627a14aa4f442f1049674fc92c90f6825
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-07-16 06:17:25 +0530

    cart api added

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('66952f337d87c6d1d382d0ba')
     .then(user => {
-      req.user = user;
+      req.user = new User (user.name,user.email , user.cart , user._id);
       next();
     })
     .catch(err => console.log(err));
