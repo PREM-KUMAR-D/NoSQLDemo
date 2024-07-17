b243aeebeea624c023e976071586ff510281fcf5
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-07-17 10:42:29 +0530

    added orders api

--- models/user.js
+++ models/user.js
@@ -71,7 +71,7 @@ class User {
 
   deleteItemFromCart(productId) {
     const updatedCartItems = this.cart.items.filter(item => {
-      return item.productId.toString() !== productId.toString();
+      return item._id.toString() !== productId.toString();
     });
     const db = getDb();
     return db
@@ -80,6 +80,37 @@ class User {
         { _id: new ObjectId(this._id) },
         { $set: { cart: {items: updatedCartItems} } }
       );
+  }
+
+  addOrder(){
+    const db = getDb();
+    return this.getCart().then(products=>{
+
+      const order ={
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+
+    .then(result =>{
+      this.cart = {items: []};
+      return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: {items: []} } }
+      );
+    })
+
+  }
+
+  getOrder(){
+    const db = getDb();
+    return db.collection('orders').find({'user._id': new ObjectId(this._id)}).toArray();
   }
 
   static findById(userId) {
